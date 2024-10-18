5bc09aca2e9775ef4200513c8646878ea48a9c9d
Mateus Oliveira <75847514+m0liveira@users.noreply.github.com>
2024-10-18 18:53:15 +0100

    style: 💄 Project finished
    Finished the UI / funcionalities of the projects page

--- Portfolio/src/app/services/project.service.ts
+++ Portfolio/src/app/services/project.service.ts
@@ -5,20 +5,22 @@ import { Injectable } from '@angular/core';
 })
 export class ProjectService {
   project: any = {
-    logo: '/assets/images/KeyHolderLogo.png',
-    title: 'KeyHolder',
-    description: 'A secure password management app for private use, designed to securely store and manage users credentials, also allowing them to generate passwords, export and import credentials into their account.',
-    descriptionShort: 'The app ensures cross-platform compatibility focusing on user security and convenience.',
-    technologies: [
-      { icon: '/assets/images/ionic.png', name: 'Ionic' },
-      { icon: '/assets/images/angular.png', name: 'Angular' },
-      { icon: '/assets/images/firebase.png', name: 'Firebase' }
-    ],
-    images: ['/assets/images/deviceframesKeyholder.png',],
+    logo: '',
+    title: '',
+    description: '',
+    descriptionShort: '',
+    technologies: [],
     isDesign: false,
     figmaUrl: '',
-    github: 'This github repository is private.',
-    githubUrl: ''
+    github: '',
+    githubUrl: '',
+    images: [],
+    date: 0,
+    collaborators: [],
+    website: '',
+    webURL: '',
+    isLaptop: false,
+    hasPhoneDemo: false,
   }
 
 
