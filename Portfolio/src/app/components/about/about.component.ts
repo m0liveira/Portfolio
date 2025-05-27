0b78c6da84bd9c6a70b87aaac1077d25c138b78b
Mateus Oliveira <75847514+m0liveira@users.noreply.github.com>
2025-05-27 02:41:03 +0100

    style: 💄 About page

--- Portfolio/src/app/components/about/about.component.ts
+++ Portfolio/src/app/components/about/about.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavigationComponent } from "../shared/navigation/navigation.component";
+import { FooterComponent } from "../shared/footer/footer.component";
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [NavigationComponent],
+  imports: [NavigationComponent, FooterComponent],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss'
 })
--- Portfolio/src/app/services/photography.service.ts
+++ Portfolio/src/app/services/photography.service.ts
@@ -4,10 +4,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class PhotographyService {
-  selectedPhoto: { src: string, alt: string } | null = {
-    src: 'https://images.unsplash.com/photo-1531632584752-b8f7bda3799d?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    alt: 'Image 1',
-  };
+  selectedPhoto: { src: string, alt: string } | null = null;
 
   constructor() { }
 
